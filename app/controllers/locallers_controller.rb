1443d687dfab160747df81e3de8f33e57e1ccb31
aika balmukhanova <abalmukh@gmail.com>
2024-06-04 14:46:05 +0200

    new localler page done

--- app/controllers/locallers_controller.rb
+++ app/controllers/locallers_controller.rb
@@ -1,2 +1,24 @@
 class LocallersController < ApplicationController
+  # uncomment below line once index and show controller actions are defined, so user who is not logged in can still see locallers
+  # skip_before_action :authenticate_user!, only: %i[index show]
+
+  def new
+    @localler = Localler.new
+  end
+
+  def create
+    @localler = Localler.new(localler_params)
+    @localler.user = current_user
+    if @localler.save
+      redirect_to localler_path(@localler)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def localler_params
+    params.require(:localler).permit(:description, :location, :availability)
+  end
 end
