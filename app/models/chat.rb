22d9da9077dafe04ff9cc5488372395e0765956f
aika balmukhanova <abalmukh@gmail.com>
2024-06-05 17:13:59 +0200

    chat done

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,3 +1,5 @@
 class Chat < ApplicationRecord
   has_many :messages
+  belongs_to :localler
+  belongs_to :user
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,18 @@
 class Message < ApplicationRecord
   belongs_to :chat
   belongs_to :user
+
+  def create
+    # need content in strong params, chat_id and user_id
+    @message = Message.new(message_params)
+    @message.user = current_user
+    @message.chat = Chat.find(params[:chat_id])
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+
 end
