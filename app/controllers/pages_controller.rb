a3647fc878ab31972d0c950b762a102ca43ffaac
Guillaume Verlooy <guillaumeverlooy@gmail.com>
2024-06-07 13:01:25 +0200

    frontend fixed in index and show pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,4 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[home]
 
-  def home
-    @home = true
-  end
-
-  def chat
-    @chat = true
-  end
 end
