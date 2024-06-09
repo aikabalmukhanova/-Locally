3689a68525416fe39432f2443e88973d34ab2b04
Mathieu Heynen <contact.heynen@gmail.com>
2024-06-09 13:28:47 +0200

    added a customize map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -13,7 +13,7 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/il-domeniddio/clx6h75pl00cs01qs5zj5db6u"
     })
 
     this.#addMarkersToMap()
@@ -38,6 +38,6 @@ export default class extends Controller {
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 3500 })
   }
 }
