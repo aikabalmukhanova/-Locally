e8b68fcd7898a6c18f096302d6e51a7aee60050d
Mathieu Heynen <contact.heynen@gmail.com>
2024-06-13 17:21:06 +0200

    trying the disconnect fix to figure if it will resolve the problem on russia load

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,17 +7,3 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
-
-//  disable send button if message content is empty
-document.addEventListener("DOMContentLoaded", function() {
-  const contentInput = document.getElementById("message-content");
-  const sendButton = document.getElementById("send-button");
-
-  contentInput.addEventListener("input", function() {
-    if (contentInput.value.trim() === "") {
-      sendButton.disabled = true;
-    } else {
-      sendButton.disabled = false;
-    }
-  });
-});
